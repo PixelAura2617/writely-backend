4cfe43b20a09fdf700190e1ae99978b6a78227b8
PixelAura2617 <pixelaura17@gmail.com>
2026-03-31 23:42:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -447,7 +447,8 @@ app.post("/update-password", async (req, res) => {
 });
 
 // ================= START =================
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("🔥 Server running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
