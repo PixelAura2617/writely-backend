b01f1cbaef16ab185f674deb7004e6ee744885db
PixelAura2617 <pixelaura17@gmail.com>
2026-04-02 13:48:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -239,7 +239,7 @@ app.post("/generate", async (req, res) => {
       },
   body: JSON.stringify({
         model: "llama-3.1-8b-instant",
-        temperature: 0.8,
+        temperature: 0.6,
         max_tokens: 800,
         messages: [
           {
